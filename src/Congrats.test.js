1d7464bca05ce26214e45993587c463c50499c48
sykog <35271885+sykog@users.noreply.github.com>
2020-02-19 10:22:43 -0800

    installed and implemented prop types

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import Enzyme, {shallow} from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
+import checkPropTypes from 'check-prop-types';
 import Congrats from "./Congrats";
-import {findByTestAttr} from "../test/utils/testUtils";
+import {findByTestAttr, checkProps} from "../test/utils/testUtils";
 
 Enzyme.configure({adapter: new EnzymeAdapter()});
 
+const defaultProps = {success: false};
 const setup = (props={}) => {
-  return shallow(<Congrats {...props} />)
+  const setupProps = {...defaultProps, ...props};
+  return shallow(<Congrats {...setupProps} />)
 };
 
 it('renders without error', () => {
-  const wrapper = setup();
+  const wrapper = setup({success: false});
   const component = findByTestAttr(wrapper, 'component-congrats');
   expect(component.length).toBe(1);
 });
@@ -24,4 +27,8 @@ it('renders non-empty congrats message when success prop is true', () => {
   const wrapper = setup({success: true});
   const message = findByTestAttr(wrapper, 'congrats-message');
   expect(message.text().length).toBeGreaterThan(0);
+});
+it('does not throw a warning with expected props', () => {
+  const expectedProps = {success: false};
+  checkProps(Congrats, expectedProps);
 });
