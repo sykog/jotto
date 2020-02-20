13b15a71532cac0cb98016225fbfee00343e87ab
sykog <35271885+sykog@users.noreply.github.com>
2020-02-19 16:20:25 -0800

    display app in App.js, some styling

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {shallow} from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders app without crashing', () => {
+  const app = shallow(<App/>);
+  expect(app.length).toBeGreaterThan(0);
 });
