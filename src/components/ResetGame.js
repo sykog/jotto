4e5846ecc91bd7c50b0f64e0130db203dac5a23a
sykog <antoniosuarez1994@gmail.com>
2020-03-18 09:52:52 -0700

    displays an error message when a user inputs an invalid word

--- src/components/ResetGame.js
+++ src/components/ResetGame.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import { resetGame } from "../actions";
 
 export class ResetGame extends Component {
-
   constructor(props) {
     super(props);
-    this.state = { resetting: false, secretWord: '' };
+    this.state = { resetting: false, secretWord: "", errorMessage: false };
     this.resetGameFromUser = this.resetGameFromUser.bind(this);
     this.resetGameFromServer = this.resetGameFromServer.bind(this);
   }
@@ -14,67 +13,88 @@ export class ResetGame extends Component {
   resetGameFromUser(event) {
     if (new RegExp("^[a-zA-Z]{5}$").test(this.state.secretWord)) {
       this.props.resetGame(this.state.secretWord);
-      this.setState({ ...this.state, resetting: false, secretWord: '' });
+      this.setState({
+        ...this.state,
+        resetting: false,
+        secretWord: "",
+        errorMessage: false
+      });
+    } else {
+      this.setState({ ...this.state, errorMessage: true });
     }
   }
 
   resetGameFromServer(event) {
     event.preventDefault();
     this.props.resetGame();
-    this.setState({ ...this.state, resetting: false });
+    this.setState({ ...this.state, resetting: false, errorMessage: false });
   }
 
   render() {
+    const errorMessage = this.state.errorMessage ? (
+      <p className="alert alert-danger" data-test="error-message">
+        Word must be 5 letters (letters only)
+      </p>
+    ) : null;
+
     const resetOptions = this.state.resetting ? (
       <React.Fragment>
+        {errorMessage}
         <div className="row mb-2">
-          <button data-test="reset-server"
+          <button
+            data-test="reset-server"
             className="btn btn-primary"
             type="text"
-            onClick={this.resetGameFromServer}>
+            onClick={this.resetGameFromServer}
+          >
             Random Word
           </button>
 
-          <input data-test="secret-word-input"
+          <input
+            data-test="secret-word-input"
             className="mx-sm-3"
             type="text"
             placeholder="Enter Secret Word"
             value={this.state.secretWord}
-            onChange={event => this.setState({ secretWord: event.target.value })}
+            onChange={event =>
+              this.setState({ secretWord: event.target.value })
+            }
           />
-          <button data-test="reset-user"
+          <button
+            data-test="reset-user"
             className="btn btn-primary"
             type="text"
-            onClick={this.resetGameFromUser}>
+            onClick={this.resetGameFromUser}
+          >
             Submit Word
           </button>
 
-          <button data-test="reset-game-cancel"
+          <button
+            data-test="reset-game-cancel"
             className="btn btn-warning mx-2"
             type="text"
             onClick={event => {
               this.setState({ ...this.state, resetting: false });
-            }}>
-            Reset Game
+            }}
+          >
+            Cancel
           </button>
         </div>
       </React.Fragment>
     ) : (
-        <button data-test="reset-game-button"
-          className="btn btn-primary mb-2"
-          type="text"
-          onClick={event => {
-            this.setState({ ...this.state, resetting: true });
-          }}>
-          Reset Game
+      <button
+        data-test="reset-game-button"
+        className="btn btn-primary mb-2"
+        type="text"
+        onClick={event => {
+          this.setState({ ...this.state, resetting: true });
+        }}
+      >
+        Reset Game
       </button>
-      );
-
-    return (
-      <div data-test="component-reset-game">
-        {resetOptions}
-      </div>
     );
+
+    return <div data-test="component-reset-game">{resetOptions}</div>;
   }
 }
 
--- src/components/ResetGame.test.js
+++ src/components/ResetGame.test.js
@@ -1,145 +1,177 @@
-import React from 'react';
-import {shallow} from "enzyme";
-import {findByTestAttr, storeFactory} from "../../test/utils/testUtils";
-import ResetGame, {ResetGame as ResetGameComponent} from "./ResetGame";
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr, storeFactory } from "../../test/utils/testUtils";
+import ResetGame, { ResetGame as ResetGameComponent } from "./ResetGame";
 
 const setup = (initialState = {}) => {
   const store = storeFactory(initialState);
   const wrapper = shallow(<ResetGame store={store} />).dive();
 
   return wrapper;
-}
+};
 
-describe('renders', () => {
+describe("renders", () => {
   let wrapper;
 
-  describe('resetting state is false', () => {
+  describe("resetting state is false", () => {
     beforeEach(() => {
       wrapper = setup();
     });
 
-    it('renders the component without error', () => {
-      const component = findByTestAttr(wrapper, 'component-reset-game');
+    it("renders the component without error", () => {
+      const component = findByTestAttr(wrapper, "component-reset-game");
       expect(component.length).toBe(1);
     });
-    it('renders the reset button', () => {
-      const resetButton = findByTestAttr(wrapper, 'reset-game-button');
+    it("renders the reset button", () => {
+      const resetButton = findByTestAttr(wrapper, "reset-game-button");
       expect(resetButton.length).toBe(1);
     });
-    it('does not render the reset server button', () => {
-      const resetButton = findByTestAttr(wrapper, 'reset-server');
+    it("does not render the reset server button", () => {
+      const resetButton = findByTestAttr(wrapper, "reset-server");
       expect(resetButton.length).toBe(0);
     });
-    it('does not render the reset user button', () => {
-      const resetButton = findByTestAttr(wrapper, 'reset-user');
+    it("does not render the reset user button", () => {
+      const resetButton = findByTestAttr(wrapper, "reset-user");
       expect(resetButton.length).toBe(0);
     });
-    it('does not render the reset user input', () => {
-      const resetButton = findByTestAttr(wrapper, 'secret-word-input');
-      expect(resetButton.length).toBe(0);
+    it("does not render the user input", () => {
+      const input = findByTestAttr(wrapper, "secret-word-input");
+      expect(input.length).toBe(0);
+    });
+    it("does not render the error message", () => {
+      const errorMessage = findByTestAttr(wrapper, "error-message");
+      expect(errorMessage.length).toBe(0);
     });
   });
 
-  describe('resetting state is true', () => {
+  describe("resetting state is true", () => {
     beforeEach(() => {
       wrapper = setup();
-      wrapper.setState({resetting: true});
+      wrapper.setState({ resetting: true });
     });
 
-    it('renders the component without error', () => {
-      const component = findByTestAttr(wrapper, 'component-reset-game');
+    it("renders the component without error", () => {
+      const component = findByTestAttr(wrapper, "component-reset-game");
       expect(component.length).toBe(1);
     });
-    it('does not render the reset button', () => {
-      const resetButton = findByTestAttr(wrapper, 'reset-game-button');
+    it("does not render the reset button", () => {
+      const resetButton = findByTestAttr(wrapper, "reset-game-button");
       expect(resetButton.length).toBe(0);
     });
-    it('renders the reset server button', () => {
-      const resetButton = findByTestAttr(wrapper, 'reset-server');
+    it("renders the reset server button", () => {
+      const resetButton = findByTestAttr(wrapper, "reset-server");
       expect(resetButton.length).toBe(1);
     });
-    it('renders the reset user button', () => {
-      const resetButton = findByTestAttr(wrapper, 'reset-user');
+    it("renders the reset user button", () => {
+      const resetButton = findByTestAttr(wrapper, "reset-user");
       expect(resetButton.length).toBe(1);
     });
-    it('renders the reset user input', () => {
-      const resetButton = findByTestAttr(wrapper, 'secret-word-input');
-      expect(resetButton.length).toBe(1);
+    it("renders the user input", () => {
+      const input = findByTestAttr(wrapper, "secret-word-input");
+      expect(input.length).toBe(1);
+    });
+
+    describe("error state is true", () => {
+      it("renders the error message", () => {
+        wrapper = setup();
+        wrapper.setState({ resetting: true, errorMessage: true });
+        const errorMessage = findByTestAttr(wrapper, "error-message");
+        console.log(wrapper.state("errorMessage"));
+
+        expect(errorMessage.length).toBe(1);
+      });
+    });
+
+    describe("error state is false", () => {
+      it("does not render the error message", () => {
+        wrapper = setup();
+        wrapper.setState({ resetting: true, errorMessage: false });
+        const errorMessage = findByTestAttr(wrapper, "error-message");
+
+        expect(errorMessage.length).toBe(0);
+      });
     });
   });
 });
 
-
-
-describe('reset buttons', () => {
+describe("reset buttons", () => {
   let resetGameMock;
   let wrapper;
   const testButtonPress = (secretWord, testId, resetting = true) => {
     resetGameMock = jest.fn();
-    wrapper = shallow(<ResetGameComponent resetGame={resetGameMock}/>);
-    wrapper.setState({secretWord, resetting});
+    wrapper = shallow(<ResetGameComponent resetGame={resetGameMock} />);
+    wrapper.setState({ secretWord, resetting });
 
     const resetButton = findByTestAttr(wrapper, testId);
-    resetButton.simulate('click', {
-      preventDefault() {
-      }
+    resetButton.simulate("click", {
+      preventDefault() {}
     });
-  }
+  };
 
-  it('sets resetting to false when the cancel button is clicked', () => {
-    testButtonPress('','reset-game-cancel');
-    expect(wrapper.state('resetting')).toBe(false);
+  it("sets resetting to false when the cancel button is clicked", () => {
+    testButtonPress("", "reset-game-cancel");
+    expect(wrapper.state("resetting")).toBe(false);
   });
 
-  it('sets resetting to true when thd reset button is clicked', () => {
-    testButtonPress('','reset-game-button', false);
-    expect(wrapper.state('resetting')).toBe(true);
+  it("sets resetting to true when thd reset button is clicked", () => {
+    testButtonPress("", "reset-game-button", false);
+    expect(wrapper.state("resetting")).toBe(true);
   });
 
-  it('calls reset game from server action when the button is clicked', () => {
-    testButtonPress('','reset-server');
+  it("calls reset game from server action when the button is clicked", () => {
+    testButtonPress("", "reset-server");
     expect(resetGameMock.mock.calls.length).toBe(1);
   });
 
-  describe('user input rested game', () => {
-
-    describe('invalid input', () => {
-      it('does not call reset on empty string', () => {
-        testButtonPress('', 'reset-user');
+  describe("user input rested game", () => {
+    describe("invalid input", () => {
+      it("does not call reset on empty string", () => {
+        testButtonPress("", "reset-user");
         expect(resetGameMock.mock.calls.length).toBe(0);
       });
 
-      it('does not call reset on word less than 5 letters', () => {
-        testButtonPress('taco', 'reset-user');
+      it("does not call reset on word less than 5 letters", () => {
+        testButtonPress("taco", "reset-user");
         expect(resetGameMock.mock.calls.length).toBe(0);
       });
 
-      it('does not call reset on word more than 5 letters', () => {
-        testButtonPress('hamburger', 'reset-user');
+      it("does not call reset on word more than 5 letters", () => {
+        testButtonPress("hamburger", "reset-user");
         expect(resetGameMock.mock.calls.length).toBe(0);
       });
 
-      it('does not call reset on word with non alpha characters', () => {
-        testButtonPress('1one!', 'reset-user');
+      it("does not call reset on word with non alpha characters", () => {
+        testButtonPress("1one!", "reset-user");
         expect(resetGameMock.mock.calls.length).toBe(0);
       });
 
-      it('does not reset the input', () => {
-        const secretWord = 'taco'
-        testButtonPress(secretWord, 'reset-user');
-        expect(wrapper.state('secretWord')).toEqual(secretWord);
+      it("does not reset the input", () => {
+        const secretWord = "taco";
+        testButtonPress(secretWord, "reset-user");
+        expect(wrapper.state("secretWord")).toEqual(secretWord);
+      });
+
+      it("sets error to true", () => {
+        const secretWord = "taco";
+        testButtonPress(secretWord, "reset-user");
+        expect(wrapper.state("errorMessage")).toBeTruthy();
       });
     });
 
-    describe('valid input', () => {
-      it('calls reset game from user action when the button is clicked', () => {
-        testButtonPress('train', 'reset-user');
+    describe("valid input", () => {
+      it("calls reset game from user action when the button is clicked", () => {
+        testButtonPress("train", "reset-user");
         expect(resetGameMock.mock.calls.length).toBe(1);
       });
 
-      it('resets the input', () => {
-        testButtonPress('train', 'reset-user');
-        expect(wrapper.state('secretWord')).toEqual('');
+      it("resets the input", () => {
+        testButtonPress("train", "reset-user");
+        expect(wrapper.state("secretWord")).toEqual("");
+      });
+
+      it("sets error to false", () => {
+        testButtonPress("train", "reset-user");
+        expect(wrapper.state("errorMessage")).toBeFalsy();
       });
     });
   });
